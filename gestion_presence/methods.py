1ca22dc86108aa9e6aade19ed090a2562d5b8faf
Frederick <frederickmugisho228@gmail.com>
2022-12-31 10:47:09 +0200

    presence app1

--- gestion_presence/methods.py
+++ gestion_presence/methods.py
@@ -41,19 +41,15 @@ def send_mail_function(subject, message, email):
         subject = f"SGP: {subject}"
         from_email = EMAIL_HOST_USER
         content_html = 'vues/send_mail_function.html'
-        print("1")
         app_host = APP_HOST
         context = {
             'message': message,
             'app_host': app_host,
         }
         html_render = render_to_string(content_html, context)
-        print("2")
         message_send = EmailMultiAlternatives(subject, "", from_email, [email])
         message_send.attach_alternative(html_render, "text/html")
         message_send.send()
-        print("3")
-        print(message_send)
     except Exception as e:
         error_html = f"<html><body><h2>Error system: {e}</h2></body></html>"
         return HttpResponse(error_html)
